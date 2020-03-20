083e1ccfc2c8b257d268b9b81cef5fce70dd8262
Maélie Lenard <maelie.lenard@gmail.com>
2020-03-20 17:01:08 +0100

    delete useless things

--- src/stories/card.stories.tsx
+++ src/stories/card.stories.tsx
@@ -1,40 +1,38 @@
 import React from 'react';
 import ImageCard from '../common/component/Card/ImageCard/index';
-// import SimpleCard from '../common/component/Card/simpleCard';
 import { withKnobs, text, select, boolean } from '@storybook/addon-knobs';
-// import { Toggle } from 'material-ui';
+import { createUseStyles } from 'react-jss';
 
 export default {
     title: 'Card',
     decorators: [withKnobs],
 };
 
+const styles = createUseStyles({
+    container: {
+        maxWidth: '50%',
+    },
+});
+
 export const imageCard = () => {
+    const classes = styles();
+
     return (
-        <ImageCard
-            image={text(
-                'Image Url',
-                'https://2.bp.blogspot.com/-ZWVaEVfnhrU/WscAG0733-I/AAAAAAAArBY/6qDxl6S4tSUGsN-g4LhG3GaXnt591kiDQCLcBGAs/s1600/scoobyNatural-warner-estreia_banner.jpg'
-            )}
-            title={text('Title', 'Scoobydoobidoooo')}
-            text={text('Text', 'You can put some text if you need')}
-            hasButton={boolean('With button ?', true)}
-            buttonText={text('Button text', 'Click here')}
-        />
+        <div className={classes.container}>
+            <ImageCard
+                image={text(
+                    'Image Url',
+                    'https://2.bp.blogspot.com/-ZWVaEVfnhrU/WscAG0733-I/AAAAAAAArBY/6qDxl6S4tSUGsN-g4LhG3GaXnt591kiDQCLcBGAs/s1600/scoobyNatural-warner-estreia_banner.jpg'
+                )}
+                title={text('Title', 'Scoobydoobidoooo')}
+                text={text(
+                    'Text',
+                    'Je me souviens en fait, après il faut s intégrer tout ça dans les environnements et c est un très, très gros travail et cette officialité peut vraiment retarder ce qui devrait devenir... Ça respire le meuble de Provence, hein ?'
+                )}
+                hasButton={boolean('With button ?', true)}
+                buttonText={text('Button text', 'Click here')}
+                altImage={text('Alt image ', 'Scoobidooooo')}
+            />
+        </div>
     );
 };
-
-// export const simpleCard = () => {
-//     const styles = ['darkTheme', 'mediumTheme', 'lightTheme'];
-
-//     return (
-//         <SimpleCard
-//             title={text('Title', 'CampusID')}
-//             style={select('style', styles)}
-//             text={text(
-//                 'Text',
-//                 'Oui alors écoute moi, premièrement, il faut se recréer... pour recréer... a better you et cette officialité peut vraiment retarder ce qui devrait devenir... Et là, vraiment, j essaie de tout coeur de donner la plus belle réponse de la terre ! '
-//             )}
-//         />
-//     );
-// };
