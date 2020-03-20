f6bbdda2dd2b2d2cfa5ec155cb443ae5afaa958a
Jan-Vermeulen <46278651+Jan-Vermeulen@users.noreply.github.com>
2020-03-20 18:24:33 +0100

    Revert "Button custom 40k1ct"

--- src/common/component/ButtonCustom/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Button from '@material-ui/core/Button';
-
-function ButtonCustom(props : any) {
-  let callBack = props.callBack;
-  return <Button variant={props.typeButton} onClick={callBack}>{props.valueButton}</Button>;
-}
-
-export default ButtonCustom;
