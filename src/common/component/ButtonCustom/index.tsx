73659e371466d9d1ed16f8b7799fe7126b48eb43
Paoli <jerome.paoli@lucielabs.com>
2020-03-20 17:35:59 +0100

    Change CustomButton and Add into

--- src/common/component/ButtonCustom/index.tsx
+++ src/common/component/ButtonCustom/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
 
-function ButtonCustom(props : any) {  
-  return (<Button variant={props.typeButton}>{props.valueButton}</Button>);
+function ButtonCustom(props : any) {
+  let callBack = props.callBack;
+  return <Button variant={props.typeButton} onClick={callBack}>{props.valueButton}</Button>;
 }
 
 export default ButtonCustom;
