0ad70e8500798650f497aa5bde873e7039eb01dd
Paoli <jerome.paoli@lucielabs.com>
2020-03-30 09:51:59 +0200

    Fx warning

--- src/common/components/sidebar/index.tsx
+++ src/common/components/sidebar/index.tsx
@@ -1,29 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import clsx from 'clsx';
-import { MenuList, MenuItem, Typography, Drawer, makeStyles, Theme, createStyles} from '@material-ui/core';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
+import { Drawer, makeStyles, Theme, createStyles} from '@material-ui/core';
 import List from '@material-ui/core/List';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Divider from '@material-ui/core/Divider';
 import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import MailIcon from '@material-ui/icons/Mail';
 
 import SettingsIcon from '@material-ui/icons/Settings';
 import SpeakerNotesIcon from '@material-ui/icons/SpeakerNotes';
-import ScheduleIcon from '@material-ui/icons/Schedule';
 import GradeIcon from '@material-ui/icons/Grade';
 import LinkIcon from '@material-ui/icons/Link';
 
-import styles from './style';
 
 const drawerWidth = 240;
 
