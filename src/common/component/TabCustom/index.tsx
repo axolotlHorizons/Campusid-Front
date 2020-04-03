1d2ee41d0bca9b7cdcf52e2ea2c9ffe661848206
Claire So <claire.sorrentino@live.com>
2020-04-03 18:13:46 +0200

    [tabcustom-40k5f2] WIP TabCustom

--- src/common/component/TabCustom/index.tsx
+++ src/common/component/TabCustom/index.tsx
@@ -3,31 +3,33 @@ import { makeStyles, Theme } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
+// import Typography from '@material-ui/core/Typography';
+// import Box from '@material-ui/core/Box';
 
-interface TabPanelProps {
-    children?: React.ReactNode;
-    index: any;
-    value: any;
-  }
+// interface TabPanelProps {
+//     children?: React.ReactNode;
+//     index: any;
+//     value: any;
+//   }
 
-function TabPanel(props: TabPanelProps) {
-  const { children, value, index, ...other } = props;
+// function TabPanel(props: TabPanelProps) {
+//   const { children, value, index, ...other } = props;
 
-  return (
-    <Typography
-      component="div"
-      role="tabpanel"
-      hidden={value !== index}
-      id={`simple-tabpanel-${index}`}
-      aria-labelledby={`simple-tab-${index}`}
-      {...other}
-    >
-      {value === index && <Box p={3}>{children}</Box>}
-    </Typography>
-  );
-}
+//   return (
+//     <Typography
+//       component="div"
+//       role="tabpanel"
+//       hidden={value !== index}
+//       id={`simple-tabpanel-${index}`}
+//       aria-labelledby={`simple-tab-${index}`}
+//       {...other}
+//     >
+//       {value === index && <Box p={3}>{children}</Box>}
+//     </Typography>
+//   );
+// }
+
+/* <TabCustom onglets={tabArray}></TabCustom> */
 
 const useStyles = makeStyles((theme: Theme) => ({
   root: {
@@ -37,35 +39,26 @@ const useStyles = makeStyles((theme: Theme) => ({
 
 function TabCustom(props : any)
 {
-    const [value, setValue] = React.useState(0);
-    const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+  const classes = useStyles();
 
-    const handleChange = (event: React.ChangeEvent<{}>, newValue: number) => {
-      setValue(newValue);
-    };
+  const handleChange = (event: React.ChangeEvent<{}>, newValue: number) => {
+    setValue(newValue);
+  };
+  
+  const printOnglet = props.onglets.map((onglet:any) =>
+    <div className="Onglet">
+      <Tabs value={value} onChange={handleChange}>
+        <Tab label={onglet}/>
+      </Tabs>
+    </div>
+  )
 
-    return (
-      <div className={classes.root}>
-        <AppBar position="static">
-          <Tabs value={value} onChange={handleChange} aria-label={props.nameSubject}>
-            for (let i = 0; i < array.length; ++i) {
-              const element = array[index];
-              
-            }
-            <Tab label={props.nameSubject}/>
-            <Tab label={props.nameSubject}/>
-            <Tab label={props.nameSubject}/>
-          </Tabs>
-        </AppBar>
-        <TabPanel value={value} index={0}>
-        Item One
-      </TabPanel>
-      <TabPanel value={value} index={1}>
-        Item Two
-      </TabPanel>
-      <TabPanel value={value} index={2}>
-        Item Three
-      </TabPanel>
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        {printOnglet}
+      </AppBar>
     </div>
   );
 }
