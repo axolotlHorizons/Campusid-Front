269f9d6134530306b160fd7f9c3242ed3995ca74
Maélie Lenard <maelie.lenard@gmail.com>
2020-04-03 16:05:29 +0200

    Add depencies for fullCalendar

--- src/common/components/Planning/index.tsx
+++ src/common/components/Planning/index.tsx
@@ -1,93 +1,77 @@
-import * as React from 'react';
+import React, { useState } from 'react';
 import FullCalendar from '@fullcalendar/react';
 import { EventInput } from '@fullcalendar/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import interactionPlugin from '@fullcalendar/interaction'; // needed for dayClick
+import styles from './style';
+import '@fullcalendar/core/main.css';
+import '@fullcalendar/daygrid/main.css';
+import '@fullcalendar/timegrid/main.css';
+import '@fullcalendar/list/main.css';
 
-// import './main.scss';
+type Props = {
+    title?: string;
+    start?: Date;
+    allDay?: String;
+};
 
 interface DemoAppState {
     calendarWeekends: boolean;
     calendarEvents: EventInput[];
 }
 
-export default class DemoApp extends React.Component<{}, DemoAppState> {
-    calendarComponentRef = React.createRef<FullCalendar>();
+export default function Planning(props: Props) {
+    const classes = styles();
 
-    constructor(props: {}) {
-        super(props);
+    const calendarComponentRef = React.createRef<FullCalendar>();
 
-        this.state = {
-            calendarWeekends: true,
-            calendarEvents: [
-                // initial event data
-                { title: 'Event Now', start: new Date() },
-            ],
+    const [calendarWeekends, setCalendarWeekends] = useState(true);
+    const [calendarEvents, setcalendarEvents] = useState([
+        {
+            title: 'Event Now',
+            start: new Date(),
+            allDay: '',
+        },
+    ]);
+
+    const toggleWeekends = () => {
+        setCalendarWeekends(!calendarWeekends);
+    };
+
+    const handleDateClick = (arg: any) => {
+        alert('here');
+        let newEvent = {
+            // creates a new array
+            title: 'New Event',
+            start: arg.date,
+            allDay: arg.allDay,
         };
-    }
 
-    render() {
-        return (
-            <div className="demo-app">
-                <div className="demo-app-top">
-                    <button onClick={this.toggleWeekends}>
-                        toggle weekends
-                    </button>
-                    &nbsp;
-                    <button onClick={this.gotoPast}>
-                        go to a date in the past
-                    </button>
-                    &nbsp; (also, click a date/time to add an event)
-                </div>
-                <div className="demo-app-calendar">
-                    <FullCalendar
-                        defaultView="dayGridMonth"
-                        header={{
-                            left: 'prev,next today',
-                            center: 'title',
-                            right:
-                                'dayGridMonth,timeGridWeek,timeGridDay,listWeek',
-                        }}
-                        plugins={[
-                            dayGridPlugin,
-                            timeGridPlugin,
-                            interactionPlugin,
-                        ]}
-                        ref={this.calendarComponentRef}
-                        weekends={this.state.calendarWeekends}
-                        events={this.state.calendarEvents}
-                        dateClick={this.handleDateClick}
-                    />
-                </div>
+        setcalendarEvents([...calendarEvents, newEvent]);
+    };
+
+    return (
+        <div className={classes.demoApp}>
+            <div className={classes.demoAppTop}>
+                <button onClick={toggleWeekends}>toggle weekends</button>
+                &nbsp; &nbsp; (also, click a date/time to add an event)
             </div>
-        );
-    }
-
-    toggleWeekends = () => {
-        this.setState({
-            // update a property
-            calendarWeekends: !this.state.calendarWeekends,
-        });
-    };
-
-    gotoPast = () => {
-        let calendarApi = this.calendarComponentRef.current!.getApi();
-        calendarApi.gotoDate('2000-01-01'); // call a method on the Calendar object
-    };
-
-    handleDateClick = (arg: any) => {
-        // if (
-        //     confirm('Would you like to add an event to ' + arg.dateStr + ' ?')
-        // ) {
-        //     this.setState({
-        //         // add new event data
-        //         calendarEvents: this.state.calendarEvents.concat({
-        //             // creates a new array
-        //             title: 'New Event',
-        //             start: arg.date,
-        //             allDay: arg.allDay,
-        //         }),
-        //     });
-    };
+            <div className={classes.demoAppCalendar}>
+                <FullCalendar
+                    defaultView="dayGridMonth"
+                    header={{
+                        left: 'prev,next today',
+                        center: 'title',
+                        right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek',
+                    }}
+                    plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+                    ref={calendarComponentRef}
+                    weekends={calendarWeekends}
+                    events={calendarEvents}
+                    dateClick={handleDateClick}
+                />
+            </div>
+        </div>
+    );
 }
