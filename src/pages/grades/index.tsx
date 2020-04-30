152d40d4a2dc35d3b7bb1db844a1d2c030aca3e6
Paoli <jerome.paoli@lucielabs.com>
2020-04-30 18:24:09 +0200

    Update index.tsx

--- src/pages/grades/index.tsx
+++ src/pages/grades/index.tsx
@@ -420,7 +420,7 @@ const Grades = () => {
                 }
                 objToPush.average = objToPush.average / matieres[y].results.length;
             }
-            matiereAverage.push(objToPush.average);
+            matiereAverage.push(objToPush);
         }
       return <GraphsContainer dataAverage={matiereAverage}/>
     };
