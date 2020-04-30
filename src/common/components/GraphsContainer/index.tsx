f038ee0a31236871edaefe7ffe240da3e73b555c
Paoli <jerome.paoli@lucielabs.com>
2020-04-30 15:10:57 +0200

    Add average and graphContainer Component

---
+++ src/common/components/GraphsContainer/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+function GraphsContainer(props : any) {
+    let dataAverage = props.dataAverage;
+    return (
+        <div>
+            Insert first graph here
+        </div>
+    );
+}
+
+export default GraphsContainer;
