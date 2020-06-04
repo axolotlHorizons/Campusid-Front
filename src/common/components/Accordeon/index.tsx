d92bc3f4fef809428f18ce81abfa79eaf3796015
Antoine BARELLI <antoinebarelli@gmail.com>
2020-06-04 12:54:59 +0200

    Ajustement Resultats Notes et Responsive

--- src/common/components/Accordeon/index.tsx
+++ src/common/components/Accordeon/index.tsx
@@ -6,9 +6,10 @@ import Typography from '@material-ui/core/Typography';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 import useStyles from './style';
-import { Card, ExpansionPanelActions, Button, Divider, CardMedia } from '@material-ui/core';
+import { Card, ExpansionPanelActions, Button, Divider, CardMedia, Grid } from '@material-ui/core';
 import { CardContent } from '@material-ui/core';
 import CustomInput from '../CustomInput';
+
 
 function Accordeon(props: any) {
     const classes = useStyles();
@@ -19,14 +20,16 @@ function Accordeon(props: any) {
     const matiereInProgress = matieres.map((matiere: any) => matiere.valid === undefined).length;
     const matiereValidation = matieres.map((matiere: any) => matiere.valid === true).length;
     const matiereCancelled = matieres.map((matiere: any) => matiere.valid === false).length;
-    const getColorTypo = (matiere : any) => {
-            return matiere.valid === true ? "primary" : matiere.valid === false ? "error" : "inherit";
+    const getColorTypo = (matiere: any) => {
+        return matiere.valid === true ? "primary" : matiere.valid === false ? "error" : "inherit";
     };
-    const getBackgroundHeading = (matiere : any) => {
-        return matiere.valid === true ? classes.backgroundValid : matiere.valid === false ? classes.backgroundInvalid : classes.heading;
+    const getBackgroundHeading = (matiere: any) => {
+        return matiere.valid === true ? classes.backgroundValid : matiere.valid === false ? classes.backgroundInvalid :
+            classes.heading;
     };
-    const getClassOfNote = (note : any) => {
-            return note.note >= 10 ? classes.details + " " + classes.backgroundValid : note.note < 10 ? classes.details + " " + classes.backgroundInvalid : classes.details;
+    const getClassOfNote = (note: any) => {
+        return note.note >= 10 ? classes.details + " " + classes.backgroundValid : note.note < 10 ? classes.details + " " +
+            classes.backgroundInvalid : classes.details;
     };
     let filtreText = (arr: any, requete: any) => {
         return arr.filter(function (el: any) {
@@ -43,135 +46,143 @@ function Accordeon(props: any) {
     };
 
     return (
-        <>
-            <div className={classes.root}>
-                <Card className={classes.root}>
-                    <div className={classes.details}>
-                        <CardContent>
-                            <div className="content">
-                                <div className="content-card-inProgress" style={{display: "inline-block", width: "25%"}}>
+    <>
+        <div className={classes.root}>
+            <Card className={classes.root}>
+                <div className={classes.details}>
+                    <CardContent>
+                        <div className="content">
 
-                                    <CardMedia style={{margin: "0px 0px 0px 40%", width: "50px", height: "50px", position: "initial"}}
-                                        image={resultsTotale.inProgress.icon}
-                                        title="En cours de validation.."
-                                    />
-                                    
-                                    <Typography style={{margin: "0px 0px 0px 20%" ,width: "25%", right: "70%", position: "initial", display: "inline"}} component="h6" variant="h6">
-                                    {resultsTotale.inProgress.name} : {matiereInProgress}
-                                    </Typography>
+                            <Grid container>
+                                <Grid item xs={12} sm container>
+                                    <Grid item container direction="column" justify="center" alignItems="center">
 
-                                </div>
-                                <div className="content-card-validated" style={{display: "inline-block", width: "25%"}}>
+                                        <Grid item>
 
-                                    <CardMedia style={{margin: "0px 0px 0px 14%", width: "50px", height: "50px", position: "initial"}}
-                                        image={resultsTotale.validated.icon}
-                                        title="Validé"
-                                    />
+                                            <CardMedia className={classes.cardImage}
+                                                image={resultsTotale.inProgress.icon}
+                                                title="En cours de validation.." />
+                                        </Grid>
 
-                                    <Typography style={{margin: "0px 0px 0px 5%", width: "25%", left: "38%", position: "initial", display: "inline"}} component="h6" variant="h6">
-                                    {resultsTotale.validated.name} : {matiereValidation}
-                                    </Typography>
+                                        <Grid item alignItems="center">
+                                            <Typography component="h6" variant="h6">
+                                                {resultsTotale.inProgress.name} : {matiereInProgress}
+                                            </Typography>
+                                        </Grid>
+                                    </Grid>
 
-                                </div>
-                                <div className="content-card-failed" style={{display: "inline-block", width: "25%"}}>
-                                    <CardMedia style={{margin: "0px 0px 0px 14%", width: "50px", height: "50px", position: "initial"}}
-                                        image={resultsTotale.failed.icon}
-                                        title="Echoué"
-                                    />
-                                
-                                    <Typography style={{margin: "0px 0px 0px 5%", width: "25%", left: "60%", position: "initial", display: "inline"}} component="h6" variant="h6">
-                                    {resultsTotale.failed.name} : {matiereCancelled}
-                                    </Typography>
-                                </div>
-                                <div className="content-card-obtainedCredits" style={{display: "inline-block", width: "25%"}}>
-                                    <CardMedia style={{margin: "0px 0px 0px 14%", width: "50px", height: "50px", position: "initial"}}
-                                        image={resultsTotale.obtainedCredits.icon}
-                                        title="Crédits"
-                                    />
+                                </Grid>
+                                <Grid item xs={12} sm container>
+                                    <Grid item container direction="column" justify="center" alignItems="center">
+                                        <Grid item>
 
-                                    <Typography style={{margin: "0px 0px 0px 5%", width: "25%", right: "10%", position: "initial", display: "inline"}} component="h6" variant="h6">
-                                    {resultsTotale.obtainedCredits.name} : 45
-                                    </Typography>
-                                </div>
-                            </div>
+                                            <CardMedia className={classes.cardImage}
+                                                image={resultsTotale.validated.icon} title="Validé" />
+                                        </Grid>
+                                        <Grid item>
+                                            <Typography component="h6" variant="h6">
+                                                {resultsTotale.validated.name} : {matiereValidation}
+                                            </Typography>
+                                        </Grid>
+                                    </Grid>
+                                </Grid>
+                            
 
-                            <CustomInput
-                                id="outlined-required"
-                                //variant="outlined"
-                                type="text"
-                                style={{ margin: 20 }}
-                                size="medium"
-                                color="secondary"
-                                placeholder="text"
-                                hasIcon={true}
-                                callBack={inputComputed}
-                                name="Matiére"
-                            />
-                        </CardContent>
-                    </div>
-                </Card>
+                                <Grid item xs={12} sm container>
+                                    <Grid item container direction="column" justify="center" alignItems="center">
+                                        <Grid item>
+                                            <CardMedia className={classes.cardImage} image={resultsTotale.failed.icon}
+                                                title="Echoué" />
+                                        </Grid>
+                                        <Grid item>
+                                            <Typography component="h6" variant="h6">
+                                                {resultsTotale.failed.name} : {matiereCancelled}
+                                            </Typography>
+                                        </Grid>
+                                    </Grid>
+                                </Grid>
 
-                {getMatieres.map(function(matiere: any, index: any) {
-                    return (
-                        <ExpansionPanel>
-                            <ExpansionPanelSummary
-                                expandIcon={<ExpandMoreIcon />}
-                                aria-controls="panel1a-content"
-                                id="panel1a-header"
-                                className={getBackgroundHeading(matiere)}
-                            >
-                                <div className={classes.column} style={{marginRight: "15px"}}>
-                                    <CardMedia
-                                        className={classes.logo}
-                                        image={matiere.icon}
-                                        title="Campus ID"
-                                    />
-                                </div>
-                                <div className={classes.column + " " + classes.marginCenterY}>
-                                    <strong>{matiere.name}</strong>
-                                </div>
+                                <Grid item xs={12} sm container>
+                                    <Grid item container direction="column" justify="center" alignItems="center">
+                                        <Grid item>
 
-                            </ExpansionPanelSummary>
+                                            <CardMedia className={classes.cardImage}
+                                                image={resultsTotale.obtainedCredits.icon} title="Crédits" />
+                                        </Grid>
+                                        <Grid item>
+                                            <Typography component="h6" variant="h6">
+                                                {resultsTotale.obtainedCredits.name} : 45
+                                            </Typography>
 
-                            {matiere.results.map(function(
-                                note: any,
-                                indexB: any
-                            ) {
-                                return (
-                                    <ExpansionPanelDetails>
-                                        <Card className={classes.root}>
-                                            <div className={classes.details}>
-                                                <CardContent className={getClassOfNote(note)} >
-                                                    <Typography component="h6" variant="h6">
-                                                       {note.name} / {note.type}
-                                                    </Typography>
-                                                    <Typography variant="subtitle1">
-                                                        Date : <strong>24/04/2020</strong>
-                                                    </Typography>
-                                                    <Typography variant="subtitle1">
-                                                        Note : <strong>{note.note}</strong>
-                                                    </Typography>
-                                                    <Typography variant="subtitle1">
-                                                        Coef : <strong>{note.coeficient}</strong>
-                                                    </Typography>
-                                                    <Typography variant="subtitle1">
-                                                        Moyenne de la classe : <strong>{note.moyenneClasse}</strong>
-                                                    </Typography>
-                                                    <Typography variant="subtitle1">
-                                                        Commentaire : <strong>{note.commentaire}</strong>
-                                                    </Typography>
-                                                </CardContent>
-                                            </div>
-                                        </Card>
-                                    </ExpansionPanelDetails>
-                                );
-                            })}
-                        </ExpansionPanel>
-                    );
+                                        </Grid>
+                                    </Grid>
+                                </Grid>
+                        </Grid>
+                        </div>
+
+                        <CustomInput id="outlined-required" //variant="outlined" type="text" style={{ margin: 20 }}
+                            size="medium" color="secondary" placeholder="text" hasIcon={true} callBack={inputComputed}
+                            name="Matiére" />
+                    </CardContent>
+                </div>
+            </Card>
+
+            {getMatieres.map(function(matiere: any, index: any) {
+            return (
+            <ExpansionPanel>
+                <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel1a-content"
+                id="panel1a-header"
+                className={getBackgroundHeading(matiere)}
+                >
+                <div className={classes.column} style={{marginRight: "15px"}}>
+                    <CardMedia className={classes.logo} image={matiere.icon} title="Campus ID" />
+                </div>
+                <div className={classes.column + " " + classes.marginCenterY}>
+                    <strong>{matiere.name}</strong>
+                </div>
+
+                </ExpansionPanelSummary>
+
+                {matiere.results.map(function(
+                note: any,
+                indexB: any
+                ) {
+                return (
+                <ExpansionPanelDetails>
+                    <Card className={classes.root}>
+                        <div className={classes.details}>
+                            <CardContent className={getClassOfNote(note)}>
+                                <Typography component="h6" variant="h6">
+                                    {note.name} / {note.type}
+                                </Typography>
+                                <Typography variant="subtitle1">
+                                    Date : <strong>24/04/2020</strong>
+                                </Typography>
+                                <Typography variant="subtitle1">
+                                    Note : <strong>{note.note}</strong>
+                                </Typography>
+                                <Typography variant="subtitle1">
+                                    Coef : <strong>{note.coeficient}</strong>
+                                </Typography>
+                                <Typography variant="subtitle1">
+                                    Moyenne de la classe : <strong>{note.moyenneClasse}</strong>
+                                </Typography>
+                                <Typography variant="subtitle1">
+                                    Commentaire : <strong>{note.commentaire}</strong>
+                                </Typography>
+                            </CardContent>
+                        </div>
+                    </Card>
+                </ExpansionPanelDetails>
+                );
                 })}
-            </div>
-        </>
+            </ExpansionPanel>
+            );
+            })}
+        </div>
+    </>
     );
-}
+    }
 
-export default Accordeon;
+    export default Accordeon;
