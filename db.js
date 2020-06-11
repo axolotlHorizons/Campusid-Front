a19c69ddf7512c5f0bd225da562e2ce817dcdfac
Jan.Vermeulen <jan.vermeulen@ricardo.ch>
2020-06-11 12:14:43 +0200

    update .env and db for testing purposes

--- db.js
+++ db.js
@@ -1,29 +1,32 @@
-const coursesJson = require ('./src/utils/jsonExample/Courses.json');
-const internshipJson = require ('./src/utils/jsonExample/Internship.json');
-const personnalInformationJson = require ('./src/utils/jsonExample/PersonnalInformation.json');
-const marksJson = require ('./src/utils/jsonExample/Marks.json');
-
+const coursesJson = require('./src/utils/jsonExample/Courses.json');
+const internshipJson = require('./src/utils/jsonExample/Internship.json');
+const personnalInformationJson = require('./src/utils/jsonExample/PersonnalInformation.json');
+const marksJson = require('./src/utils/jsonExample/Marks.json');
 
 module.exports = () => {
     return {
-        "student": [
+        student: [
             {
-                "id": 1,
-                "name": "Fname",
-                "firstname": "Lname",
-                "avatar": "https://thispersondoesnotexist.com/image"
-            }
+                id: 1,
+                classId: 1,
+                lastname: 'Lastname',
+                firstname: 'Firstname',
+                avatar: 'https://thispersondoesnotexist.com/image',
+                email: 'test1@campusid.eu',
+                password: '1234',
+                role: 'student',
+            },
         ],
-        "comments": [
+        comments: [
             {
-                "id": 1,
-                "body": "some comment",
-                "postId": 1
-            }
+                id: 1,
+                body: 'some comment',
+                postId: 1,
+            },
         ],
-        "profile": personnalInformationJson,
-        "grades": marksJson,
-        "internship": internshipJson,
-        "courses": coursesJson
+        profile: personnalInformationJson,
+        grades: marksJson,
+        internship: internshipJson,
+        courses: coursesJson,
     };
 };
