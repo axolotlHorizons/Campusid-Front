3c7d1a620c955b7fc10b800dbad6e038ca76aa40
Maélie Lenard <maelie.lenard@gmail.com>
2020-06-11 09:39:30 +0200

    add domain et aspect

--- src/common/components/RadarChartContainer/index.tsx
+++ src/common/components/RadarChartContainer/index.tsx
@@ -1,14 +1,19 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import * as am4core from '@amcharts/amcharts4/core';
 import * as am4charts from '@amcharts/amcharts4/charts';
 import am4themes_dataviz from '@amcharts/amcharts4/themes/dataviz';
 import am4themes_animated from '@amcharts/amcharts4/themes/animated';
 import styles from './style';
+import ButtonCustom from './../ButtonCustom/index';
 
 function RadarChartContainer(props: any) {
     const classes = styles();
-    let dataAverage = props.dataAverage;
-    console.log(dataAverage);
+    const dataAverage = props.dataAverage;
+    const aspectAverage = props.aspectAverage;
+    const coursesAverage = props.coursesAverage;
+
+    const [dataChart, setdataChart] = useState(dataAverage);
+    const [chart, setChart] = useState<am4charts.RadarChart>();
 
     useEffect(() => {
         /* Chart code */
@@ -17,35 +22,59 @@ function RadarChartContainer(props: any) {
         am4core.useTheme(am4themes_animated);
         // Themes end
 
-        let chart = am4core.create('chartdiv', am4charts.RadarChart);
-        chart.hiddenState.properties.opacity = 0; // this creates initial fade-in
-        chart.data = dataAverage;
+        let chartTmp = am4core.create('chartdiv', am4charts.RadarChart);
+        chartTmp.hiddenState.properties.opacity = 0; // this creates initial fade-in
+        chartTmp.data = dataAverage;
 
-        let categoryAxis = chart.xAxes.push(
+        let categoryAxis = chartTmp.xAxes.push(
             new am4charts.CategoryAxis() as any
         );
         categoryAxis.dataFields.category = 'name';
 
-        let valueAxis = chart.yAxes.push(new am4charts.ValueAxis() as any);
-        valueAxis.renderer.axisFills.template.fill = chart.colors.getIndex(2);
+        let valueAxis = chartTmp.yAxes.push(new am4charts.ValueAxis() as any);
+        valueAxis.renderer.axisFills.template.fill = chartTmp.colors.getIndex(
+            2
+        );
         valueAxis.renderer.axisFills.template.fillOpacity = 0.05;
         valueAxis.renderer.maxLabelPosition = 0.99;
 
         /* Create and configure series */
-        let series = chart.series.push(new am4charts.RadarSeries());
+        let series = chartTmp.series.push(new am4charts.RadarSeries());
         series.dataFields.valueY = 'average';
         series.dataFields.categoryX = 'name';
         series.name = 'Notes';
         series.strokeWidth = 2;
 
         /* Add cursor */
-        chart.cursor = new am4charts.RadarCursor();
+        chartTmp.cursor = new am4charts.RadarCursor();
 
-        chart.legend = new am4charts.Legend();
-    }, [dataAverage]);
+        chartTmp.legend = new am4charts.Legend();
+        setChart(chartTmp);
+    }, []);
+
+    const aspectChart = () => {
+        let chartTmp: any = chart || {};
+        chartTmp.data = aspectAverage;
+        setChart(chartTmp);
+    };
+
+    const domainChart = () => {
+        let chartTmp: any = chart || {};
+        chartTmp.data = dataChart;
+        setChart(chartTmp);
+    };
+
+    const coursesChart = () => {
+        let chartTmp: any = chart || {};
+        chartTmp.data = coursesAverage;
+        setChart(chartTmp);
+    };
 
     return (
         <div>
+            <ButtonCustom valueButton="Domaine" callBack={domainChart} />
+            <ButtonCustom valueButton="Aspect" callBack={aspectChart} />
+            <ButtonCustom valueButton="Cours" callBack={coursesChart} />
             <div id="chartdiv" className={classes.graphStyle}></div>
         </div>
     );
